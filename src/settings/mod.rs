97a95331cd17bc9be63997ed8e6c5f85f0074dcd
FreePhoenix <freephoenix888@gmail.com>
2024-02-18 00:12:58 +0600

    Add working way with the same folder

--- src/settings/mod.rs
+++ src/settings/mod.rs
@@ -1,5 +1,3 @@
-mod storage_settings;
-
 use serde::{Deserialize, Serialize};
 use prost::Message;
 
@@ -8,3 +6,7 @@ pub mod settings {
     include!(concat!(env!("OUT_DIR"), "/settings.rs"));
 }
 
+// Include the `storage_settings` module, which is generated from settings.proto.
+pub mod storage_settings {
+    include!(concat!(env!("OUT_DIR"), "/storage_settings.rs"));
+}
--- src/settings/storage_settings/mod.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-use prost::Message;
-
-// Include the `storage_settings` module, which is generated from storage_settings.proto.
-pub mod storage_settings {
-    include!(concat!(env!("OUT_DIR"), "/storage_settings.rs"));
-}
-
